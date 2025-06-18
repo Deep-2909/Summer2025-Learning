32740b5d29cfcfd5d4c1883d99066bdd6ad47403
Deepanshu Sharma <sharmadeepanshu1412@gmail.com>
2025-06-19 00:16:34 +0530

    Added LeetCode solutions: Two Sum and Search in Rotated Array

--- DSA/Arrays/twoSum.cpp
+++ DSA/Arrays/twoSum.cpp
@@ -1,1 +1,24 @@
-// LeetCode 1: Two Sum solution
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            std::vector<int> v;
+            int n = nums.size();
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(i == j)
+                        continue;
+                    if(nums[i] + nums[j] == target)
+                    {
+                        v.push_back(i);
+                        v.push_back(j);
+                        break;
+                    }
+                }
+                if(v.size() == 2)
+                    break;
+            }
+            return v;
+        }
+    };
